42c6f712354291e40106379363c3a6bed594c68b
Sorin Chis <kish.sorin@yahoo.com>
2019-07-23 17:20:19 +0300

    finally fixing bug on node verison - with mongoDB

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 require("dotenv").config();
-const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
+const { ApolloServer } = require("apollo-server");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-mongoose
-  .connect(
-    "mongodb+srv://sorin:<test123456>@geoapp-zjuzv.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDb connected"))
-  .catch(err => console.log(err));
+// mongoose
+//   .connect(
+//     "mongodb+srv://Scama:test123@geoapp-zjuzv.mongodb.net/test?retryWrites=true&w=majority",
+//     { useNewUrlParser: true }
+//   )
+//   .then(() => console.log("MongoDb connected"))
+//   .catch(err => console.log(err));
+
+mongoose.connect(
+  "mongodb://Scama:test123@geoapp-shard-00-00-zjuzv.mongodb.net:27017,geoapp-shard-00-01-zjuzv.mongodb.net:27017,geoapp-shard-00-02-zjuzv.mongodb.net:27017/test?ssl=true&replicaSet=GeoApp-shard-0&authSource=admin&retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("Connected to Database Woop Woop");
+});
 
 const server = new ApolloServer({
   typeDefs,
