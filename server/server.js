5998b6f295fd9efcce0b826d08bd2aa0a95cc6a5
Sorin Chis <kish.sorin@yahoo.com>
2019-07-23 16:32:43 +0300

    added mongo db connection to Atlas

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
+require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
 
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
+
+mongoose
+  .connect(
+    "mongodb+srv://sorin:<test123456>@geoapp-zjuzv.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDb connected"))
+  .catch(err => console.log(err));
 
 const server = new ApolloServer({
   typeDefs,
