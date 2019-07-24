eac4cb9cbaf10c0a848c307df1e3f225efdf1d25
Sorin Chis <kish.sorin@yahoo.com>
2019-07-24 16:40:35 +0300

    added GraphQl Clinet for sendinf the request and context for getting the request from the header

--- server/server.js
+++ server/server.js
@@ -5,14 +5,6 @@ const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://Scama:test123@geoapp-zjuzv.mongodb.net/test?retryWrites=true&w=majority",
-//     { useNewUrlParser: true }
-//   )
-//   .then(() => console.log("MongoDb connected"))
-//   .catch(err => console.log(err));
-
 mongoose.connect(
   "mongodb://Scama:test123@geoapp-shard-00-00-zjuzv.mongodb.net:27017,geoapp-shard-00-01-zjuzv.mongodb.net:27017,geoapp-shard-00-02-zjuzv.mongodb.net:27017/test?ssl=true&replicaSet=GeoApp-shard-0&authSource=admin&retryWrites=true&w=majority",
   { useNewUrlParser: true }
@@ -23,7 +15,18 @@ mongoose.connection.once("open", () => {
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: ({ req }) => {
+    let authToken = null;
+    try {
+      authToken = req.headers.authorization;
+      if (authToken) {
+        //find and create user
+      }
+    } catch (error) {
+      console.error(`Unable to authentificate user with token ${authToken}`);
+    }
+  }
 });
 
 server.listen().then(({ url }) => {
