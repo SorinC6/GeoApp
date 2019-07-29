5da59cca4963f28eb08222c4f300b1c04988adb9
Sorin Chis <kish.sorin@yahoo.com>
2019-07-29 11:27:22 +0300

    added content in NoContent Component

--- client/src/components/Pin/NoContent.js
+++ client/src/components/Pin/NoContent.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { withStyles } from "@material-ui/core/styles";
-// import Explore from "@material-ui/icons/Explore";
-// import Typography from "@material-ui/core/Typography";
+import ExploreIcon from "@material-ui/icons/Explore";
+import Typography from "@material-ui/core/Typography";
 
-const NoContent = ({ classes }) => <div>NoContent</div>;
+const NoContent = ({ classes }) => (
+  <div className={classes.root}>
+    <ExploreIcon className={classes.icon} />
+    <Typography
+      noWrap
+      component="h2"
+      variant="h6"
+      align="center"
+      color="textPrimary"
+      gutterBottom
+    >
+      Click on the map to add a pin
+    </Typography>
+  </div>
+);
 
 const styles = theme => ({
   root: {
