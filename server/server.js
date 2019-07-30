2a10de38118ef2efe11641648375c3948f7ece33
Sorin Chis <kish.sorin@yahoo.com>
2019-07-30 16:00:57 +0300

    Heroku deployment

--- server/server.js
+++ server/server.js
@@ -33,6 +33,6 @@ const server = new ApolloServer({
   }
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server is listening on : ${url}`);
 });
